528fb08b01ad50e77bba11ff91b859f8237e5828
Marcelo <riverasoto.marcelo@gmail.com>
2023-12-20 16:31:01 -0600

    sixth commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        
+      </head>
       <body className={inter.className}>
         <Appbar/>
         
